251c29689279e9fd07f58eaede1106680e8ce855
AustinBH <harlowaustin2@gmail.com>
2019-06-09 15:24:18 -0700

    added option to edit artist bio and updated show pages to link to edit page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,7 +24,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(artist_params(:name))
+    @artist.update(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
 
